467d01b6ea7b6be99fca089e2503ca1075a6852f
davidbudac <davidbudac@gmail.com>
2023-10-22 15:04:56 +0200

    Basic aliases and settings for environments without OCW

---
+++ bash_profile_no-ocw.sh
@@ -0,0 +1,36 @@
+# if rlwrap exists, use it for everything
+if command -v rlwrap &> /dev/null; then
+  alias sqlplus='rlwrap sqlplus'
+  alias rman='rlwrap rman'
+  alias dgmgrl='rlwrap dgmgrl'
+fi
+
+alias sps='sqlplus / as sysdba'
+alias lsl='lsl -altr'
+
+# silly attempt to extract unique name from the spfile
+# and set path to alertlog
+function get_oracle_db_unique_name {
+    ORACLE_SPFILE_PATH="$ORACLE_HOME/dbs/spfile$ORACLE_SID.ora"
+
+    # Check if the SPFILE exists
+    if [ -f "$ORACLE_SPFILE_PATH" ]; then
+        # Attempt to extract db_unique_name from spfile
+        ORACLE_DB_UNIQUE_NAME=$(strings $ORACLE_SPFILE_PATH | grep "\*\.db_unique_name" | grep -o -P "'.*?'" | sed "s/'//g")
+        # Check if db_unique_name was found
+        if [ -n "$ORACLE_DB_UNIQUE_NAME" ]; then
+            echo "\$ORACLE_DB_UNIQUE_NAME set to: $ORACLE_DB_UNIQUE_NAME"
+            export ald="$ORACLE_BASE/diag/rdbms/$ORACLE_DB_UNIQUE_NAME/$ORACLE_SID/trace"
+            export al="$ald/alert_$ORACLE_SID.log"
+            alias alog='tail -f -n 100 $al'
+        else
+            echo "Error: Unable to extract DB_UNIQUE_NAME from SPFILE."
+        fi
+    else
+        echo "Error: SPFILE not found at $ORACLE_SPFILE_PATH"
+    fi
+}
+
+# oraenv, ignore all the commented lines and blank lines
+# then set the path to alert log
+alias oe='cat /etc/oratab | grep -vE "^(#|$)" ; . oraenv ; get_oracle_db_unique_name'
